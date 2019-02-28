9b74e12297aacf741aa9f21365ea0bf700a7af31
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2019-02-28 20:31:21 +0100

    Toevoegen aan slideshow indien score onder 5

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "photo.h"
 #include "Slide.h"
+#include "evaluation.h"
 
 int main(int argc, char* argv[]) {
 
@@ -13,6 +14,9 @@ int main(int argc, char* argv[]) {
   std::cin >> N;
   std::vector<Photo> photosV;
   std::vector<Photo> photosH;
+  std::vector<Photo> rejected_H;
+
+  Slide slideshow();
   for (int n {0}; n < N; ++n) {
     char orientation;
     int tag_count;
@@ -32,9 +36,25 @@ int main(int argc, char* argv[]) {
       std::cin >> curr_tag;
       photosH[photosH.size()-1].add_tag(curr_tag);
       // TODO: toevoegen aan stack en vergelijken
+
+      // Als er nog geen slides zijn
+      if(slideshow().isEmpty()){
+        slideshow().add_slide(photosH[photosH.size()]);
+
+        // Als de match score kleiner is dan 5
+      } else if (evaluation::score(slideshow().last_slide().tags, photosH[photosH.size()-1].tags) < 5){
+        slideshow().add_slide(photosH[photosH.size()-1]);
+
+        // Als de match verwerpt wordt
+      } else {
+        rejected_H.emplace_back(photosH[photosH.size()-1]);
+      }
+
     }
   }
   }
+
+
 
   // Onze verzameling zit nu in photos
 
