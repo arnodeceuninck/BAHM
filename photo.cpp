faba6abafb123c027a07c14213fb1ec5bb008438
hxyxxn <hayaanabdi96+github@gmail.com>
2019-02-28 19:36:30 +0100

    Update photo.cpp

--- photo.cpp
+++ photo.cpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <unordered_set>
 
-Photo::Photo()
+Photo::Photo() = default;
+
+Photo::Photo(int id, char orientation)
   : id(id), orientation(orientation) {}
 
 void Photo::add_tag(const std::string& tag) { tags.emplace(tag); }
