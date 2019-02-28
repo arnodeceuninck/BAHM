34942b79d2c6be50fffbe8f492bd8586b29b9f39
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2019-02-28 20:55:12 +0100

    extra comments

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@
 #include "Slide.h"
 #include "evaluation.h"
 
+using  namespace std;
+
 int main(int argc, char* argv[]) {
 
   // Inlezen van het bestand
+  cout << "Start reading..." << endl;
 
   int N;
   std::cin >> N;
@@ -17,7 +20,10 @@ int main(int argc, char* argv[]) {
   std::vector<Photo> rejected_H;
 
   Slide slideshow = Slide({});
+
+  cout << "voor for loop" << endl;
   for (int n {0}; n < N; ++n) {
+      cout << "in for loop" << endl;
     char orientation;
     int tag_count;
     std::cin >> orientation >> tag_count;
@@ -43,10 +49,13 @@ int main(int argc, char* argv[]) {
 
         // Als de match score kleiner is dan 5
       } else if (evaluation::score(slideshow.last_slide().tags, photosH[photosH.size()-1].tags) < 5){
+        cout << "slide added" << endl;
+
         slideshow.add_slide(photosH[photosH.size()-1]);
 
         // Als de match verwerpt wordt
       } else {
+        cout << "slide rejected" << endl;
         rejected_H.emplace_back(photosH[photosH.size()-1]);
       }
 
@@ -56,6 +65,7 @@ int main(int argc, char* argv[]) {
 
   int try_count = 0;
   while (try_count < 100 and rejected_H.size() > 0){
+      cout << "Trying to optimize: try " << try_count << endl;
     try_count++;
     std::vector<Photo> rejected_H_2 = {};
 
@@ -72,10 +82,11 @@ int main(int argc, char* argv[]) {
     rejected_H = rejected_H_2;
   }
 
+  cout << "Adding rejected photos" << endl;
   for(auto photo: rejected_H){
     slideshow.add_slide(photo);
   }
-  
+
   // Vanaf hier zit de te outputten data in slideshow
 
 
